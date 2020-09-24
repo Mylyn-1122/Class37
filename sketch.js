2f8873fed7fcbd792415bf05d16fcb34d8b96559
Mylyn-1122 <26zhengmylyn@gmail.com>
2020-09-23 19:53:47 -0700

    database

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
-var ball;
+var ball, database, position;
+
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var loc = database.ref("ball/position");
+    loc.on("value", readOp);
 }
 
 function draw(){
@@ -24,6 +28,16 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref("ball/position").set({
+        x : ball.x + x,
+        y : ball.y + y
+    })
+    
+
+}
+
+function readOp(data){
+    position = data.val()
+    ball.x = position.x;
+    ball.y = position.y;
 }
