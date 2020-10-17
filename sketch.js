d98bfdc921bec88dd35d8fd746985aab1129ba37
Mylyn-1122 <69164770+Mylyn-1122@users.noreply.github.com>
2020-10-16 23:34:24 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,112 +1,48 @@
-var bananaImage, obstacleImage, backImage, monkeyanime;
-var obstacleGroup, bananaGroup;
-var background_, monkey;
-var score;
+var canvas, backgroundImage;
 
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
 
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var car1Img, car2Img, car3Img, car4Img, trackImg, groundImg
 
 function preload(){
-  bananaImage = loadImage("banana.png");
-  
-  obstacleImage = loadImage("stone.png");
-  
-  monkeyanime = loadAnimation ("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
-  
-  backImage = loadImage("jungle.jpg");
-  
+  car1Img = loadImage("images/car1.png")
+  car2Img = loadImage("images/car2.png")
+  car3Img = loadImage("images/car3.png")
+  car4Img = loadImage("images/car4.png")
+  trackImg = loadImage("images/track.jpg")
+  groundImg = loadImage("images/ground.png")
 }
 
-function setup() {
-  createCanvas(displayWidth/2, displayHeight/2 - 50);
-  
-  background_ = createSprite(300, 120, 20, 20);
-  background_.addImage(backImage);
-  background_.x = background_.width/2;
-  
-  monkey = createSprite(100, 240, 20, 20);
-  monkey.addAnimation("monkeyimage",monkeyanime);
-  monkey.scale = 0.10;
-  monkey.velocityX = 3;
-  
-  ground = createSprite(300, 290, 600, 10);
-  ground.visible = false;
-  
-  obstacleGroup = createGroup();
-  bananaGroup = createGroup();
-  
-  score = 0;
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
 
-function draw() {
-  background(255);
 
-  camera.position.x = monkey.x + 200;
-  camera.position.y = displayHeight/2 -200;
-
-  ground.x = monkey.x;
-  
-  if(background_.x < 0){
-    background_.x = background_.width/2;
+function draw(){
+  if(playerCount === 4){
+    clear()
+    game.update(1);
+    
   }
-  
-  if(keyDown("space") && monkey.y > 230){
-    monkey.velocityY = -15;
+  if(gameState === 1){
+    clear();
+    game.play();
   }
-  
-  monkey.collide(ground);
-  monkey.velocityY = monkey.velocityY + 0.5;
-  
-  if(bananaGroup.isTouching(monkey)){
-    bananaGroup.destroyEach();
-    score = score + 2;
+  //text(mouseX + " " + mouseY, mouseX, mouseY)
+  if(gameState === 2){
+    clear()
+    game.end()
+    
   }
-  
-  if(obstacleGroup.isTouching(monkey)){
-    monkey.scale = 0.10;
-    score = 0; 
-  }
-  
-  switch(score){
-      case 10: monkey.scale = 0.12;
-      break;
-      case 20: monkey.scale = 0.14;
-      break;
-      case 30: monkey.scale = 0.16;
-      break;
-      case 40: monkey.scale = 0.18;
-      break;
-      default:break;}
-  
-  spawnObstacle();
-  
-  spawnBanana();
-  
-  drawSprites();
-  
-  stroke("white");
-  textSize(20);
-  fill("black");
-  text("SCORE : " + score, monkey.x + 300, 50);
-  
-  //console.log(monkey.y)
 }
-function spawnObstacle(){
-  if(frameCount % 300 === 0){
-    var obstacle = createSprite(monkey.x + 500, 270, 40, 40);
-    obstacle.velocityX = -3;
-    obstacle.lifetime = 150;
-    obstacleGroup.add(obstacle);
-    obstacle.scale = 0.15;
-    obstacle.addImage(obstacleImage);
-  }}
-
-function spawnBanana(){
-  if(frameCount % 80 === 0){
-    var banana = createSprite(monkey.x + 500, 270, 40, 40);
-    banana.y = Math.round(150, 200);
-    banana.velocityX = -2;
-    banana.lifetime = 120;
-    bananaGroup.add(banana);
-    banana.scale = 0.05 ;
-    banana.addImage(bananaImage);
-  }}
